8ca731be59a9e185fad0dfce229b021705385abc
jaffarabbas <fa19bsse0008@maju.edu.pk>
2021-09-27 01:34:17 +0500

    27/09/2021 01:34:16

---
+++ Auto-Commit-Work/main.py
@@ -0,0 +1,69 @@
+from git import Repo
+from git.db import GitCmdObjectDB
+from datetime import datetime
+
+
+class RepoUrls:
+    file = open('repo-links.txt', 'r')
+    fileList = []
+
+    def PopulateList(self):
+        for filePath in self.file.read().split('\n'):
+            self.fileList.append(filePath)
+
+
+class Router(RepoUrls):
+    count = 0
+    character = ''
+    path = ''
+
+    def ChooseRoute(self):
+        self.PopulateList()
+        print("choose repo : ")
+        for i in self.fileList:
+            self.count += 1
+            print(self.count, i)
+
+        while True:
+            self.character = input("Enter Repo Number: ")
+            if self.character == 'X':
+                break
+            else:
+                self.path = self.fileList[int(self.character) - 1]
+                break
+
+
+class GitFunctions:
+    route = Router()
+    route.ChooseRoute()
+    repo = Repo(route.path, odbt=GitCmdObjectDB)
+    origin = repo.remote('origin')
+
+    def GitCommandRunner(self, count, commit, commit_message):
+        self.repo.index.add(commit)
+        self.repo.index.commit(commit_message)
+        print(f'{count} : committed file : {commit}')
+
+
+def main():
+    # commit object
+    commit = GitFunctions()
+    # counter for counter committed files
+    count = 0
+    # commit message
+    commit_message = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    # add files to stage and commit
+    for i in commit.repo.untracked_files:
+        commit.GitCommandRunner(count, i, commit_message)
+        count += 1
+    for item in commit.repo.index.diff(None):
+        commit.GitCommandRunner(count, item.a_path, commit_message)
+        count += 1
+    # push all commits at once
+    commit.origin.push()
+
+    print(f"Done !!! committing : {count} files")
+
+
+if __name__ == '__main__':
+    main()
